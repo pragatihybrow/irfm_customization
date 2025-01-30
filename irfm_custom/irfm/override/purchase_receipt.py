8ddf46cc67a2e6379f33b22bcba7da535d7bc92f
Pragati <pragati@mail.hybrowlabs.com>
2025-01-30 15:45:17 +0530

    |irfm_custom|created purchase recipt with barcode|

--- irfm_custom/irfm/override/purchase_receipt.py
+++ irfm_custom/irfm/override/purchase_receipt.py
@@ -1,29 +1,33 @@
 import frappe
+from frappe import _
 
 @frappe.whitelist()
 def create_purchase_invoice_from_grn(doc, method):
-    # Create a new Shipment
-    shipment = frappe.new_doc("Purchase Invoice")
-    shipment.supplier = supplier  # Link the Shipment to the Delivery Note
-    shipment.company = doc.company
-    # Append the shipment delivery note
+    # Create a new Purchase Invoice
+    grn = frappe.new_doc("Purchase Invoice")
+    
+    # Link the Purchase Invoice to the Supplier from the Purchase Receipt
+    grn.supplier = doc.supplier  # Use doc.supplier, not 'supplier'
+    grn.company = doc.company
+    
+    # Append the items from the Purchase Receipt to the Purchase Invoice
     for item in doc.items:
-        sales_invoice.append("items", {
+        grn.append("items", {
             "item_code": item.item_code,
             "qty": item.qty,
-            "custom_box_barcode":item.custom_box_barcode,
+            "custom_box_barcode": item.custom_box_barcode,
             "uom": item.uom,
             "warehouse": item.warehouse,
             "rate": item.rate,
             "amount": item.amount,
             "description": item.description,
             "cost_center": item.cost_center,
-            "delivery_note":doc.name,
+            "purchase_receipt":doc.name
         })
 
-    # Add taxes from Delivery Note (if applicable)
+    # Add taxes from the Purchase Receipt (if applicable)
     for tax in doc.taxes:
-        sales_invoice.append("taxes", {
+        grn.append("taxes", {
             "charge_type": tax.charge_type,
             "account_head": tax.account_head,
             "description": tax.description,
@@ -31,10 +35,9 @@ def create_purchase_invoice_from_grn(doc, method):
             "tax_amount": tax.tax_amount
         })
 
-    # Save and submit the Sales Invoice
-    sales_invoice.save(ignore_permissions=True)
-    sales_invoice.submit()
+    # Save and submit the Purchase Invoice
+    grn.save(ignore_permissions=True)
+    grn.submit()
 
     # Notify the user
-    frappe.msgprint(_("Sales Invoice {0} created successfully from Delivery Note {1}").format(sales_invoice.name, doc.name))
-
+    frappe.msgprint(_("Purchase Invoice {0} created successfully from {1} Purchase Receipt").format(grn.name, doc.name))
