2c5dbac5ebef638052ca416ec8312a7bb7d1ca8a
Pragati <pragati@mail.hybrowlabs.com>
2025-02-03 17:25:56 +0530

    |irfm_custom|added connection of sales order on delivery note|

--- irfm_custom/irfm/override/delivery_note.py
+++ irfm_custom/irfm/override/delivery_note.py
@@ -18,33 +18,97 @@ form_grid_templates = {"items": "templates/form_grid/item_grid.html"}
 
 
 
+# @frappe.whitelist()
+# def create_sales_invoice_from_delivery_note(doc, method):
+#     # Create a new Sales Invoice
+#     sales_invoice = frappe.new_doc("Sales Invoice")
+#     sales_invoice.customer = doc.customer
+#     sales_invoice.company = doc.company
+#     sales_invoice.due_date = frappe.utils.add_days(frappe.utils.nowdate(), 30)  # Example: set due date 30 days from now
+#     sales_invoice.is_pos = 0  # Set this to 1 if it's a Point of Sale invoice
+#     sales_invoice.delivery_note = doc.name  # Link to the submitted Delivery Note
+
+#     # Transfer items from the Delivery Note to the Sales Invoice
+#     for item in doc.items:
+#         sales_invoice.append("items", {
+#             "item_code": item.item_code,
+#             "qty": item.qty,
+#             # "has_item_scanned":item.has_item_scanned,
+#             "uom": item.uom,
+#             "warehouse": item.warehouse,
+#             "rate": item.rate,
+#             "amount": item.amount,
+#             "description": item.description,
+#             "cost_center": item.cost_center,
+#             "delivery_note":doc.name,
+#         })
+
+#     # Add taxes from Delivery Note (if applicable)
+#     for tax in doc.taxes:
+#         sales_invoice.append("taxes", {
+#             "charge_type": tax.charge_type,
+#             "account_head": tax.account_head,
+#             "description": tax.description,
+#             "rate": tax.rate,
+#             "tax_amount": tax.tax_amount
+#         })
+
+#     # Save and submit the Sales Invoice
+#     sales_invoice.save(ignore_permissions=True)
+#     sales_invoice.submit()
+
+#     # Notify the user
+#     frappe.msgprint(_("Sales Invoice {0} created successfully from Delivery Note {1}").format(sales_invoice.name, doc.name))
+
+import frappe
+from frappe import _
+
 @frappe.whitelist()
 def create_sales_invoice_from_delivery_note(doc, method):
+    """Create a Sales Invoice when a Delivery Note is submitted."""
+
     # Create a new Sales Invoice
     sales_invoice = frappe.new_doc("Sales Invoice")
     sales_invoice.customer = doc.customer
     sales_invoice.company = doc.company
-    sales_invoice.due_date = frappe.utils.add_days(frappe.utils.nowdate(), 30)  # Example: set due date 30 days from now
+    sales_invoice.due_date = frappe.utils.add_days(frappe.utils.today(), 30)  # Set due date 30 days from now
     sales_invoice.is_pos = 0  # Set this to 1 if it's a Point of Sale invoice
     sales_invoice.delivery_note = doc.name  # Link to the submitted Delivery Note
 
-    # Transfer items from the Delivery Note to the Sales Invoice
+    # Transfer items from Delivery Note to Sales Invoice
     for item in doc.items:
-        sales_invoice.append("items", {
+        # Fetch the original Sales Order if available
+        sales_order_doc = None
+        if item.against_sales_order:
+            sales_order_doc = frappe.get_doc("Sales Order", item.against_sales_order)
+
+        # Find the correct Sales Order Item ID (so_detail) if available
+        sales_order_item_id = None
+        if sales_order_doc:
+            for soi in sales_order_doc.items:
+                if soi.item_code == item.item_code:
+                    sales_order_item_id = soi.name  # Sales Order Item ID
+                    break  # Stop once found
+
+        # Append item to Sales Invoice
+        sales_invoice_item = sales_invoice.append("items", {
             "item_code": item.item_code,
             "qty": item.qty,
-            # "has_item_scanned":item.has_item_scanned,
             "uom": item.uom,
             "warehouse": item.warehouse,
             "rate": item.rate,
             "amount": item.amount,
             "description": item.description,
             "cost_center": item.cost_center,
-            "delivery_note":doc.name,
+            "delivery_note": doc.name,  # Link to Delivery Note
+            "dn_detail": item.name,  # Link to Delivery Note Item ID
+            "sales_order": item.against_sales_order,  # Ensure Sales Order reference
+            "so_detail": sales_order_item_id,  # Link to correct Sales Order Item ID
         })
 
+
     # Add taxes from Delivery Note (if applicable)
-    for tax in doc.taxes:
+    for tax in doc.get("taxes", []):
         sales_invoice.append("taxes", {
             "charge_type": tax.charge_type,
             "account_head": tax.account_head,
@@ -53,8 +117,8 @@ def create_sales_invoice_from_delivery_note(doc, method):
             "tax_amount": tax.tax_amount
         })
 
-    # Save and submit the Sales Invoice
-    sales_invoice.save(ignore_permissions=True)
+    # Save and Submit the Sales Invoice
+    sales_invoice.insert(ignore_permissions=True)
     sales_invoice.submit()
 
     # Notify the user
--- irfm_custom/irfm/override/pick_list.py
+++ irfm_custom/irfm/override/pick_list.py
@@ -101,52 +101,6 @@ def create_delivery_note_from_picklist(doc, method):
     frappe.msgprint(_("Delivery Note {0} created successfully").format(delivery_note.name))
 
 
-# @frappe.whitelist()
-# def create_delivery_note_from_picklist(doc, method):
-#     # Create a new Delivery Note
-#     delivery_note = frappe.new_doc("Delivery Note")
-#     delivery_note.pick_list = doc.name  # Link the delivery note to the Pick List
-#     delivery_note.customer = doc.customer
-#     delivery_note.set_warehouse = doc.parent_warehouse
-#     delivery_note.company = doc.company
-#     delivery_note.delivery_date = frappe.utils.today()  # Set delivery date as today's date
-    
-#     # Get the first item to set custom_sales_order from
-#     if doc.locations:
-#         first_item = doc.locations[0]
-#         sales_order_doc = frappe.get_doc("Sales Order", first_item.sales_order)
-#         delivery_note.custom_sales_order = first_item.sales_order  # Set custom_sales_order from first item
-    
-#     delivery_note.taxes_and_charges = sales_order_doc.taxes_and_charges
-
-#     # Store tax details from Sales Order
-#     for item in doc.locations:
-#         sales_order_doc = frappe.get_doc("Sales Order", item.sales_order)
-
-#         delivery_note.append("items", {
-#             "item_code": item.item_code,
-#             "qty": item.picked_qty,
-#             "uom": item.stock_uom,
-#             "warehouse": item.warehouse,
-#             "custom_box_barcode": item.custom_barcode,
-#             "so_detail":item.sales_order,
-#             "against_sales_order":item.sales_order
-#         })
-
-#         # Fetch taxes_and_charges from Sales Order
-#         for tax_entry in sales_order_doc.get('taxes'):
-#             delivery_note.append("taxes", {
-#                 "charge_type": tax_entry.charge_type,
-#                 "account_head": tax_entry.account_head,
-#                 "description": tax_entry.description,
-#                 "rate": tax_entry.rate,
-#                 "tax_amount": tax_entry.tax_amount
-#             })
-
-#     delivery_note.save(ignore_permissions=True)
-#     frappe.msgprint(_("Delivery Note {0} created successfully").format(delivery_note.name))
-
-
 @frappe.whitelist()
 def get_status(doc, docstatus, update_modified=True):
     if doc.docstatus == 0:
